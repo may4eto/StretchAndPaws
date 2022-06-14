a581fe2be9b9e2f9941ac97f73ed9f84813aff11
Maya Mircheva <may4eto@hotmail.com>
2022-06-14 22:23:33 +0200

    added timer panel

--- Stretch+Paws/DetailView.swift
+++ Stretch+Paws/DetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DetailView: View {
     let pose: Pose
+    @State private var timerOpen = false
     var body: some View {
         ZStack {
             Color("Secondary").ignoresSafeArea()
@@ -42,6 +43,10 @@ struct DetailView: View {
                     }
                 }.padding(.horizontal, 20)
             }
+            TimerPanelView(timerOpen: $timerOpen)
+        }
+        .onTapGesture {
+            timerOpen = false
         }
     }
 }
@@ -56,5 +61,38 @@ struct DetailView_Previews: PreviewProvider {
             "Did someone say dog? Can't we call this a downward-facing cat instead? It's OK – this is a friendly dog, it's not interested in chasing cats. In fact, Downward-Facing Dog is the lynchpin of a yoga asana practice: if you're going to befriend with any of these poses, make sure it's this canine classic.",
             steps: ["From a kneeling position, place your hands shoulder-distance apart and spread your fingers.", "Tuck your toes and lift your hips up towards the ceiling so you create an inverted V shape.", "Balance the weight between hands and feet and think about tilting your tailbone up towards the ceiling.","Send your gaze towards your feet and breath!"],
             topTip: "Bend your knees in order to create more length through the spine." ))
+    }
+}
+
+struct TimerPanelView: View {
+    @Binding var timerOpen: Bool
+    var body: some View {
+        VStack {
+            Spacer()
+            VStack {
+                timerOpen ? AnyView(TimerOpenView()) : AnyView(TimerClosedView())
+            }
+            .foregroundColor(Color("Secondary"))
+            .frame(maxWidth: .infinity, maxHeight: timerOpen ? 200 : 80)
+            .background(Color("Highlight"))
+            .cornerRadius(20)
+        }
+        .ignoresSafeArea()
+        .onTapGesture {
+            timerOpen.toggle()
+        }
+    }
+}
+
+
+struct TimerOpenView: View {
+    var body: some View {
+            Text("Hold the pose")
+    }
+}
+
+struct TimerClosedView: View {
+    var body: some View {
+        Text("Try it out")
     }
 }
