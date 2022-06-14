cca35bf4a768b8caf1899a839bb4bd00bca73fb2
Maya Mircheva <may4eto@hotmail.com>
2022-06-14 15:30:26 +0200

    Initial Commit

---
+++ Stretch+Paws/Stretch_PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stretch_PawsApp.swift
+//  Stretch+Paws
+//
+//  Created by Maya Mircheva on 14/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Stretch_PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
