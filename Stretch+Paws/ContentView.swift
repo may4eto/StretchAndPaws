98d21b688b55440138af070ce5cd093a46a012c3
Maya Mircheva <may4eto@hotmail.com>
2022-06-15 18:59:59 +0200

    added splash screen

--- Stretch+Paws/ContentView.swift
+++ Stretch+Paws/ContentView.swift
@@ -12,23 +12,37 @@ struct ContentView: View {
         UITableView.appearance().backgroundColor = UIColor(named: "Secondary")
     }
     let poses = Poses()
+    @State var isActive:Bool = false
     var body: some View {
-        NavigationView {
-            List(poses.poseData) {pose in
-                NavigationLink(destination: DetailView(pose: pose)) {
-                    Image(pose.icon)
-                        .resizable()
-                        .frame(width: 60, height: 60)
-                    Text(pose.name)
-                        .fontWeight(.medium)
-                        .font(.title3)
-                        .padding(.leading, 20)
+        VStack {
+            if self.isActive {
+                NavigationView {
+                List(poses.poseData) {pose in
+                    NavigationLink(destination: DetailView(pose: pose)) {
+                        Image(pose.icon)
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                        Text(pose.name)
+                            .fontWeight(.medium)
+                            .font(.title3)
+                            .padding(.leading, 20)
+                    }
+                    .padding(5)
+                    .listRowBackground(Color("Secondary"))
                 }
-                .padding(5)
-                .listRowBackground(Color("Secondary"))
+                .listStyle(.grouped)
+                .navigationBarTitle("Stretch + Paws")
+                }
+            } else {
+                SplashView()
             }
-            .listStyle(.grouped)
-            .navigationBarTitle("Stretch + Paws")
+        }
+        .onAppear() {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                withAnimation(Animation.easeInOut(duration: 2)) {
+                    isActive = true
+                }
+            }
         }
     }
 }
@@ -36,5 +50,19 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SplashView: View {
+    var body: some View {
+        ZStack {
+            Color("Secondary").ignoresSafeArea()
+            VStack {
+                Image("Cat-3")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .foregroundColor(Color("HightLight"))
+            }
+        }
     }
 }
